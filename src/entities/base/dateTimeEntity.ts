5f289c830e46e7b9d011458c98dc7a16339de771
Irene Guijarro <iguijarro44@gmail.com>
2020-02-20 22:57:25 +0100

    Boilerplate Express + Typescript + MySql + TypeORM + JWT

---
+++ src/entities/base/dateTimeEntity.ts
@@ -0,0 +1,9 @@
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export class DateTimeEntity {
+  @CreateDateColumn({nullable: true})
+  createdAt: Date;
+
+  @UpdateDateColumn({nullable: true})
+  updatedAt: Date;
+}
